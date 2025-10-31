3118940c3a04a5abbb4a586ccd7509ed4cb9d06e
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-10-31 15:20:21 +0900

    IDLE class 생성.

--- player.py
+++ player.py
@@ -4,12 +4,18 @@ from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDL_KEY
 import game_world
 
 
+class Idle:
+    pass
+
+
 class Player:
     def __init__(self):
         self.x, self.y = 400, 400
         self.frame = 0
         self.face_dir = 1
         self.image = load_image('player.png')
+        
+        self.IDLE = Idle(self)
 
     def update(self):
         pass
