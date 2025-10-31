d896dcb069c0fda69b1f9f2a9d33e4e5d5afc5ef
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-10-31 15:09:01 +0900

    remove object 함수 생성.

--- game_world.py
+++ game_world.py
@@ -6,6 +6,14 @@ def add_object(o,depth):
 def add_objects(ol,depth):
     world[depth] += ol
 
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+
+    raise Exception("World 에 존재하지 않는 오브젝트입니다.")
+
 def update():
     for layer in world:
         for o in layer:
