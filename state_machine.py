71ed5238e51eb95b902e21b34349b083e8a5ba42
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-10-31 15:29:18 +0900

    StateMachine 클래스 함수 추가.

--- state_machine.py
+++ state_machine.py
@@ -1,4 +1,14 @@
 from event_to_string import event_to_string
 
 class StateMachine:
-    pass
+    def __init__(self, start_state, state_transitions):
+        pass
+
+    def update(self):
+        pass
+
+    def handle_state_event(self, event):
+        pass
+
+    def draw(self):
+        pass
