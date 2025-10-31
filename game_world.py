606866bfb70520cee2da5eb1370517f3e225f550
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-10-31 15:05:18 +0900

    render 함수 생성.

--- game_world.py
+++ game_world.py
@@ -5,4 +5,8 @@ def update():
     for layer in world:
         for o in layer:
             o.update()
-            
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
