2886179a0338c2756640425aead082b08231b4f5
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-10-31 15:18:57 +0900

    player class 기본틀 구현.

--- player.py
+++ player.py
@@ -1,2 +1,21 @@
+from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDL_KEYUP
+
+import game_world
+
+
 class Player:
-    pass
+    def __init__(self):
+        self.x, self.y = 400, 400
+        self.frame = 0
+        self.face_dir = 1
+        self.image = load_image('player.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def handle_event(self):
+        pass
