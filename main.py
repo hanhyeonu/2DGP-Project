4dad28b73f726cea03af8c3fd97eaefc0c8e9b6a
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-11-07 16:28:00 +0900

    canvas 크기 조정.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pico2d import open_canvas, close_canvas
 import game_framework
 import play_mode as start_mode
 
-open_canvas(800, 600)
+open_canvas(1200, 900)
 game_framework.run(start_mode)
 close_canvas()
 
