4bca5621181b1b2ae9a781adb32db8ce8a966385
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-11-07 16:23:42 +0900

    player.py 수정. 시간에 맞춰

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from pico2d import load_image, get_time
 from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN, SDL_KEYUP
 
 import game_world
+import game_framework
 from state_machine import StateMachine
 
 
@@ -37,12 +38,17 @@ def down_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
 
 
-def any_key_down(e):
-    return right_down(e) or left_down(e) or up_down(e) or down_down(e)
+# Player Speed
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel = 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-
-def any_key_up(e):
-    return right_up(e) or left_up(e) or up_up(e) or down_up(e)
+# Player Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 10
 
 
 class Idle:
@@ -58,64 +64,55 @@ class Idle:
         pass
 
     def do(self):
-        self.player.frame = (self.player.frame + 1) % 2
+        self.player.frame = (self.player.frame + 2 * ACTION_PER_TIME * game_framework.frame_time) % 2
 
     def draw(self):
         if self.player.face_dir == 1:  # 오른쪽
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 272, 28, 32, 0, '', self.player.x,
-                                                  self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-272, 28, 32, 0, '', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == 2:  # 우상
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 400, 28, 32, 0, '', self.player.x,
-                                                  self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-400, 28, 32, 0, '', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == 3:  # 우하
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 336, 28, 32, 0, '', self.player.x,
-                                                  self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-336, 28, 32, 0, '', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == -1:  # 왼쪽
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 272, 28, 32, 0, 'h',
-                                                  self.player.x, self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-272, 28, 32, 0, 'h', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == -2:  # 좌상
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 400, 28, 32, 0, 'h',
-                                                  self.player.x, self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-400, 28, 32, 0, 'h', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == -3:  # 좌하
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 338, 28, 32, 0, 'h',
-                                                  self.player.x, self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-336, 28, 32, 0, 'h', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == 0:  # 아래
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 303, 28, 32, 0, '', self.player.x,
-                                                  self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-303, 28, 32, 0, '', self.player.x, self.player.y, 56, 64)
         elif self.player.face_dir == 4:  # 위
-            self.player.image.clip_composite_draw(self.player.frame * 28 + 450, 512 - 368, 28, 32, 0, '', self.player.x,
-                                                  self.player.y, 56, 64)
+            self.player.image.clip_composite_draw(int(self.player.frame) * 28 + 450, 512-368, 28, 32, 0, '', self.player.x, self.player.y, 56, 64)
 
 
 class Run:
     # 빨간색 박스로 표시된 정확한 스프라이트 좌표
-    # 각 줄마다 48픽셀 높이, 10개의 스프라이트(각 48픽셀 너비)
     SPRITE_COORDS = {
-        1: {  # 오른쪽 (1줄)
+        1: {  # 오른쪽
             'y': 464,
             'height': 48,
             'x': [0, 48, 96, 144, 192, 240, 288, 336, 384, 432],
             'width': [48, 48, 48, 48, 48, 48, 48, 48, 48, 48]
         },
-        0: {  # 아래 (2줄)
+        0: {  # 아래
             'y': 416,
             'height': 48,
             'x': [0, 48, 96, 144, 192, 240, 288, 336, 384, 432],
             'width': [48, 48, 48, 48, 48, 48, 48, 48, 48, 48]
         },
-        3: {  # 우하 (3줄)
+        3: {  # 우하
             'y': 368,
             'height': 48,
             'x': [0, 48, 96, 144, 192, 240, 288, 336, 384, 432],
             'width': [48, 48, 48, 48, 48, 48, 48, 48, 48, 48]
         },
-        4: {  # 위 (4줄)
+        4: {  # 위
             'y': 320,
             'height': 48,
             'x': [0, 48, 96, 144, 192, 240, 288, 336, 384, 432],
             'width': [48, 48, 48, 48, 48, 48, 48, 48, 48, 48]
         },
-        2: {  # 우상 (5줄)
+        2: {  # 우상
             'y': 272,
             'height': 48,
             'x': [0, 48, 96, 144, 192, 240, 288, 336, 384, 432],
@@ -127,7 +124,6 @@ class Run:
         self.player = player
 
     def enter(self, e):
-        # 키 입력에 따라 방향 업데이트
         if right_down(e):
             self.player.dir_x += 1
         elif left_down(e):
@@ -146,60 +142,54 @@ class Run:
         elif down_up(e):
             self.player.dir_y += 1
 
-        # dir_x, dir_y에 따라 face_dir 설정 (8방향)
         self.update_face_dir()
 
     def exit(self, e):
         pass
 
     def update_face_dir(self):
-        if self.player.dir_x > 0:  # 오른쪽
+        """현재 dir_x, dir_y에 따라 face_dir 업데이트"""
+        if self.player.dir_x > 0:
             if self.player.dir_y > 0:
                 self.player.face_dir = 2  # 우상
             elif self.player.dir_y < 0:
                 self.player.face_dir = 3  # 우하
             else:
                 self.player.face_dir = 1  # 오른쪽
-        elif self.player.dir_x < 0:  # 왼쪽
+        elif self.player.dir_x < 0:
             if self.player.dir_y > 0:
                 self.player.face_dir = -2  # 좌상
             elif self.player.dir_y < 0:
                 self.player.face_dir = -3  # 좌하
             else:
                 self.player.face_dir = -1  # 왼쪽
-        else:  # dir_x == 0
+        else:
             if self.player.dir_y > 0:
                 self.player.face_dir = 4  # 위
             elif self.player.dir_y < 0:
                 self.player.face_dir = 0  # 아래
 
     def do(self):
-        self.player.frame = (self.player.frame + 1) % 10  # 각 줄마다 10개의 스프라이트
+        self.player.frame = (self.player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 10
 
-        # 대각선 이동시 속도 조정 (피타고라스)
-        speed = 3
+        # 대각선 이동시 속도 정규화
+        speed = RUN_SPEED_PPS * game_framework.frame_time
         if self.player.dir_x != 0 and self.player.dir_y != 0:
-            # 대각선 이동시 sqrt(2)로 나눠서 속도 정규화
-            speed = speed * 0.7071  # 1/sqrt(2) ≈ 0.7071
+            speed = speed * 0.7071  # 1/sqrt(2)
 
         self.player.x += self.player.dir_x * speed
         self.player.y += self.player.dir_y * speed
 
-        # 방향 업데이트 (키가 변경되었을 수 있음)
         self.update_face_dir()
 
     def draw(self):
-        # face_dir에 따라 적절한 방향 선택
-        # 음수는 반전
         direction = abs(self.player.face_dir)
         flip = 'h' if self.player.face_dir < 0 else ''
 
-        # 해당 방향의 좌표 정보 가져오기
         if direction in self.SPRITE_COORDS:
             coords = self.SPRITE_COORDS[direction]
-            frame = self.player.frame
+            frame = int(self.player.frame)
 
-            # 프레임이 범위 내에 있는지 확인
             if frame < len(coords['x']):
                 x = coords['x'][frame]
                 y = coords['y']
@@ -207,11 +197,11 @@ class Run:
                 height = coords['height']
 
                 self.player.image.clip_composite_draw(
-                    x, y,  # 소스 이미지의 x, y
-                    width, height,  # 소스 이미지의 width, height
-                    0, flip,  # 회전, 반전
-                    self.player.x, self.player.y,  # 화면 위치
-                    width * 2, height * 2  # 화면 크기 (2배)
+                    x, y,
+                    width, height,
+                    0, flip,
+                    self.player.x, self.player.y,
+                    width * 2, height * 2
                 )
 
 
@@ -230,10 +220,12 @@ class Player:
             self.IDLE,
             {
                 self.IDLE: {
-                    any_key_down: self.RUN
+                    right_down: self.RUN, left_down: self.RUN,
+                    up_down: self.RUN, down_down: self.RUN
                 },
                 self.RUN: {
-                    any_key_up: self.IDLE
+                    right_up: self.IDLE, left_up: self.IDLE,
+                    up_up: self.IDLE, down_up: self.IDLE
                 }
             }
         )
@@ -241,11 +233,8 @@ class Player:
     def update(self):
         self.state_machine.update()
 
-    def draw(self):
-        self.state_machine.draw()
-
     def handle_event(self, event):
-        # 이벤트 처리 - RUN 상태에서도 키 입력을 받아서 방향을 업데이트
+        # RUN 상태에서도 키 입력을 받아서 방향을 업데이트
         if self.state_machine.cur_state == self.RUN:
             if right_down(('INPUT', event)):
                 self.dir_x = min(self.dir_x + 1, 1)
@@ -266,3 +255,6 @@ class Player:
                 self.dir_y = min(self.dir_y + 1, 1)
 
         self.state_machine.handle_state_event(('INPUT', event))
+
+    def draw(self):
+        self.state_machine.draw()
