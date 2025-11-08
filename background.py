2ae78f91b15052ba89766ccddebd986610940d3d
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-11-08 19:26:58 +0900

    background 구현. (마을 맵만)

---
+++ background.py
@@ -0,0 +1,14 @@
+from pico2d import load_image
+
+
+class Background:
+    def __init__(self):
+        self.image = load_image('kyoten.png')
+        self.canvas_width = 800
+        self.canvas_height = 600
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.canvas_width // 2, self.canvas_height // 2, self.canvas_width, self.canvas_height)
