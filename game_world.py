40040135e97a45af8162887a8b2a4f1ad9ebb586
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-11-28 15:33:55 +0900

    game_world 수정

--- game_world.py
+++ game_world.py
@@ -1,10 +1,10 @@
 world = [[] for _ in range(4)]
+collision_pairs = {}
 
-def add_object(o, depth=0):
+def add_object(o, depth = 0):
     world[depth].append(o)
 
-
-def add_objects(ol, depth=0):
+def add_objects(ol, depth = 0):
     world[depth] += ol
 
 
@@ -22,11 +22,19 @@ def render(camera=None):
             else:
                 o.draw()
 
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+
 
 def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
             return
     raise ValueError('Cannot delete non existing object')
 
@@ -34,18 +42,35 @@ def remove_object(o):
 def clear():
     for layer in world:
         layer.clear()
+    collision_pairs.clear()
 
 
 def collide(a, b):
-    """두 객체의 바운딩 박스가 겹치는지 검사"""
     left_a, bottom_a, right_a, top_a = a.get_bb()
     left_b, bottom_b, right_b, top_b = b.get_bb()
 
-    # 겹치지 않는 4가지 경우
     if left_a > right_b: return False
     if right_a < left_b: return False
     if top_a < bottom_b: return False
     if bottom_a > top_b: return False
 
-    # 위 4가지가 모두 아니면 충돌
     return True
+
+
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        print(f'Added new group {group}')
+        collision_pairs[group] = [ [], [] ]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def handle_collisions():
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
