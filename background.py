034bb5ec05b390ba681fb316dec0a44cd1d91608
Hanhyeonu(desktop) <hhw3078@gmail.com>
2025-11-29 16:43:36 +0900

    통합한 배경그리기

--- background.py
+++ background.py
@@ -3,18 +3,105 @@ from pico2d import load_image
 
 class Background:
     def __init__(self):
+        # 기존 배경 이미지
         self.kyoten_image = load_image('kyoten.png')
         self.grass_tile = load_image('base_grass_cc.png')
+
+        # 타일맵 이미지 로드
+        self.tile_images = {
+            'ground1': load_image('ground1.png'),
+            'ground2': load_image('ground2.png'),
+            'ground3': load_image('ground3.png'),
+            'wall1': load_image('wall1.png'),
+            'wall2': load_image('wall2.png')
+        }
+
+        # 기존 배경 설정
         self.canvas_width = 2048
         self.canvas_height = 2048
         self.tile_size = 32
-
         self.grass_height = int(self.canvas_height * 0.7)
+
+        # 타일맵 설정
+        self.TILE_SIZE = 32  # 원본 이미지 크기
+        self.TILE_SCALE = 4  # 4배 확대
+        self.SCALED_TILE_SIZE = self.TILE_SIZE * self.TILE_SCALE  # 128x128
+        self.MAP_WIDTH = 1024
+        self.MAP_HEIGHT = 1024
+        self.TILES_X = self.MAP_WIDTH // self.SCALED_TILE_SIZE  # 8개
+        self.TILES_Y = self.MAP_HEIGHT // self.SCALED_TILE_SIZE  # 8개
+
+        # 맵 데이터 정의 (8x8 2차원 배열)
+        # 0: ground, 1: wall
+
+        # 맵1: ground1 + wall1
+        self.map1 = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1]
+        ]
+
+        # 맵2: ground2 + wall2
+        self.map2 = [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1]
+        ]
+
+        # 맵3: ground3만 (벽 없음)
+        self.map3 = [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+        # 맵4: 기존 배경 (kyoten + grass)
+        self.map4 = None  # None이면 기존 배경 사용
+
+        # 현재 맵 설정
+        self.current_map = self.map1
+        self.current_map_number = 1
+        self.use_tilemap = True  # True면 타일맵, False면 기존 배경
+
+    def set_map(self, map_number):
+        """맵 전환"""
+        if map_number == 1:
+            self.current_map = self.map1
+            self.current_map_number = 1
+            self.use_tilemap = True
+        elif map_number == 2:
+            self.current_map = self.map2
+            self.current_map_number = 2
+            self.use_tilemap = True
+        elif map_number == 3:
+            self.current_map = self.map3
+            self.current_map_number = 3
+            self.use_tilemap = True
+        elif map_number == 4:
+            self.current_map = self.map4
+            self.current_map_number = 4
+            self.use_tilemap = False  # 기존 배경 사용
 
     def update(self):
         pass
 
-    def draw(self, camera=None):
+    def draw_original_background(self, camera):
+        """기존 배경 그리기 (kyoten + grass)"""
         tiles_x = self.canvas_width // self.tile_size + 1
         tiles_y = self.grass_height // self.tile_size + 1
 
@@ -45,4 +132,49 @@ class Background:
                         self.tile_size
                     )
 
-            self.kyoten_image.draw(self.canvas_width // 2, self.canvas_height // 2, self.canvas_width, self.canvas_height)
+            self.kyoten_image.draw(self.canvas_width // 2, self.canvas_height // 2, self.canvas_width,
+                                   self.canvas_height)
+
+    def draw_tilemap(self, camera):
+        """타일맵 그리기 (맵1, 2, 3)"""
+        for y in range(self.TILES_Y):
+            for x in range(self.TILES_X):
+                tile_value = self.current_map[y][x]
+
+                # 월드 좌표 계산 (왼쪽 하단 기준)
+                world_x = x * self.SCALED_TILE_SIZE + self.SCALED_TILE_SIZE // 2
+                world_y = y * self.SCALED_TILE_SIZE + self.SCALED_TILE_SIZE // 2
+
+                # 카메라 적용
+                if camera:
+                    draw_x, draw_y = camera.apply(world_x, world_y)
+                else:
+                    draw_x, draw_y = world_x, world_y
+
+                # 타일 그리기
+                if self.current_map_number == 1:
+                    # 맵1: ground1 + wall1
+                    if tile_value == 0:
+                        self.tile_images['ground1'].draw(draw_x, draw_y, self.SCALED_TILE_SIZE, self.SCALED_TILE_SIZE)
+                    elif tile_value == 1:
+                        self.tile_images['wall1'].draw(draw_x, draw_y, self.SCALED_TILE_SIZE, self.SCALED_TILE_SIZE)
+
+                elif self.current_map_number == 2:
+                    # 맵2: ground2 + wall2
+                    if tile_value == 0:
+                        self.tile_images['ground2'].draw(draw_x, draw_y, self.SCALED_TILE_SIZE, self.SCALED_TILE_SIZE)
+                    elif tile_value == 1:
+                        self.tile_images['wall2'].draw(draw_x, draw_y, self.SCALED_TILE_SIZE, self.SCALED_TILE_SIZE)
+
+                elif self.current_map_number == 3:
+                    # 맵3: ground3만
+                    self.tile_images['ground3'].draw(draw_x, draw_y, self.SCALED_TILE_SIZE, self.SCALED_TILE_SIZE)
+
+    def draw(self, camera=None):
+        """통합 렌더링"""
+        if self.use_tilemap:
+            # 타일맵 모드 (맵1, 2, 3)
+            self.draw_tilemap(camera)
+        else:
+            # 기존 배경 모드 (맵4)
+            self.draw_original_background(camera)
